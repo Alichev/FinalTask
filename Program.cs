5aac73d100ce2e9e5efc211b249a5b97ab621569
Anton Alichev <tosiakz@yandex.ru>
2022-11-08 20:34:41 +0300

    add solution

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Из имеющегося массива строк сформировать массив, длина строк которого меньше или равна 3 символам.
+using static System.Console;
+
+Clear();
+string[] array = { "x2", "Hello", "Monday", "17/238", "2", ":-)" };
+PrintArray(array);
+string[] arrayNew = new string[array.Length];
+getArrayOf3Elements(array, arrayNew, 3);
+arrayNew = arrayNew.Except(new List<string> { string.Empty }).ToArray();
+PrintArray(arrayNew);
+
+
+
+
+void getArrayOf3Elements(string[] array1, string[] array2, int maxLength)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= maxLength)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
